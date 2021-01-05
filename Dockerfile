1eda33bb479bcd03d38585fbe956ddf2868e55a1
root <hon-y_leong@live.com.sg>
2021-01-05 16:40:07 +1100

    New files

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM centos
+WORKDIR /root
+RUN yum -y install epel-release glibc-langpack-en
+RUN yum -y install --enablerepo powertools wget python3 python3-devel perl gcc-c++ lmdb-devel gnutls-devel gpgme-devel jansson-devel libarchive-devel libacl-devel openldap-devel pam-devel krb5-server texinfo zlib-devel perl-Parse-Yapp dbus-devel krb5-devel rpcgen
+RUN wget https://download.samba.org/pub/samba/samba-4.13.3.tar.gz
+RUN tar xvf samba-4.13.3.tar.gz
+WORKDIR /root/samba-4.13.3
+RUN ["./configure", "--prefix=/usr/local", "--localstatedir=/var", "--sysconfdir=/etc", "--with-system-mitkrb5", "--with-experimental-mit-ad-dc", "--without-systemd", "--enable-fhs"]
+RUN make
+RUN make install
+WORKDIR /root
+COPY smb.sh .
+CMD ["sh", "smb.sh"]
---
+++ smb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ ! -f /var/lib/samba/private/sam.ldb ]
+then
+    if [[ -n $REALM && -n $DOMAIN && -n $ADMINPASS && -n $DNSFORWARDER ]]
+    then
+        echo Setting up Samba Domain Controller...
+        samba-tool domain provision --use-rfc2307 --domain=$DOMAIN --realm=$REALM --adminpass=$ADMINPASS
+        cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
+        sed -i "s/dns forwarder = .*/dns forwarder = $DNSFORWARDER/g" /etc/samba/smb.conf
+        cp /etc/samba/smb.conf /var/lib/samba/private
+        echo Starting Samba Domain Controller...
+        samba -F
+    else
+        echo Please define REALM, DOMAIN and ADMINPASS environment variable to setup Samba Domain Controller. 
+        exit -1
+    fi
+else
+    echo Starting Samba Domain Controller...
+    cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
+    cp /var/lib/samba/private/smb.conf /etc/samba
+    samba -F
+fi
